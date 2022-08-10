60f1c684d482af598459d3a8a98fc832efb2e4a7
Benny-Sankevich <68659790+Benny-Sankevich@users.noreply.github.com>
2022-08-10 11:40:34 +0300

    1

---
+++ backend/Movies.DTO/BaseDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace Movies.DTO
+{
+    #region BaseDTO
+    public class BaseDTO
+    {
+        public string Id { get; set; }
+    }
+    #endregion
+
+    #region MovieListResponseDTO
+    public class MovieListResponseDTO
+    {
+        public MovieListResponseDTO()
+        {
+            this.Search = new List<MovieDTO>();
+        }
+        public List<MovieDTO> Search { get; set; }
+        public string totalResults { get; set; }
+    }
+    #endregion
+}
---
+++ backend/Movies.Services.Contracts/IMovieService.cs
@@ -0,0 +1,22 @@
+﻿using Movies.DTO;
+using Movies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Movies.Models.BaseModels;
+
+namespace Movies.Services.Contracts
+{
+    public interface IMovieService
+    {
+        #region GetMovies
+        Task<MovieListResponseDTO> GetMovies(RequestListParameters request);
+        #endregion
+
+        #region GetMovieDetails
+        Task<MovieDetailsDTO> GetMovieDetails(RequestDetailsParameters request);
+        #endregion
+    }
+}

8c8fdc00d5e14f5bb3733d95ac74cc8986535bd5
Benny-Sankevich <68659790+Benny-Sankevich@users.noreply.github.com>
2022-08-10 11:43:51 +0300

    2

---
+++ backend/Movies.Models/BaseModels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Models
+{
+    public class BaseModels
+    {
+        #region RequestListParameters
+        public class RequestListParameters
+        {
+            public int pageNo { get; set; }
+            public string? searchText { get; set; }
+        }
+        #endregion
+
+        #region RequestDetailsParameters
+        public class RequestDetailsParameters
+        {
+            public string movieId { get; set; }
+        }
+        #endregion
+
+        #region MovieListResponse
+        public class MovieListResponse
+        {
+            public List<Movie> Search { get; set; }
+            public string totalResults { get; set; }
+            public string Response { get; set; }
+        }
+        #endregion
+
+    }
+}
